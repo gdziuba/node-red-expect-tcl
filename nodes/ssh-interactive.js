9947cb3571836b31eb7e9c403dcc029208ce7a62
gdziuba <gmdziuba@gmail.com>
2024-02-13 12:01:31 -0500

    added stricthostkeychecking

--- nodes/ssh-interactive.js
+++ nodes/ssh-interactive.js
@@ -8,6 +8,14 @@ module.exports = function(RED) {
 
         function connectSSH(sshConfig) {
             var conn = new Client();
+
+            // Apply custom SSH configurations
+            // For example, to ignore strict host key checking
+            if (config.ignoreHostKey) {
+                sshConfig.hostHash = 'sha256'; // ssh2 requires hostHash if hostVerifier is used
+                sshConfig.hostVerifier = (hashedKey, callback) => callback(true);
+            }
+
             conn.on('ready', () => {
                 node.log('SSH Client Ready');
                 isConnected = true;
@@ -36,27 +44,28 @@ module.exports = function(RED) {
                 node.error('SSH Client Error: ' + err.toString());
                 isConnected = false;
                 node.status({fill: "red", shape: "ring", text: "disconnected"});
-            }).connect(sshConfig);
+            });
+
+            // Prepare and connect with the provided sshConfig
+            conn.connect(sshConfig);
         }
 
         node.on('input', function(msg) {
-            // Preprocess privateKey to handle potential newline character formatting in both msg.payload and static config
             const dynamicPrivateKey = msg.payload.privateKey ? msg.payload.privateKey.replace(/\\n/g, '\n') : null;
             const staticPrivateKey = config.privateKey ? config.privateKey.replace(/\\n/g, '\n') : null;
-        
+
             const sshConfig = {
                 host: msg.payload.host || config.host,
                 port: msg.payload.port || config.port || 22,
                 username: msg.payload.username || config.username,
-                password: msg.payload.password || config.password, // Fallback to password if privateKey is not provided
+                password: msg.payload.password || config.password,
                 privateKey: dynamicPrivateKey ? Buffer.from(dynamicPrivateKey, 'utf-8') : (staticPrivateKey ? Buffer.from(staticPrivateKey, 'utf-8') : undefined),
-                passphrase: msg.payload.passphrase || config.passphrase // If your private key is passphrase-protected, handle it accordingly
+                passphrase: msg.payload.passphrase || config.passphrase
             };
 
             if (!isConnected) {
                 connectSSH(sshConfig); // Connect using prioritized parameters
             }
-            // If already connected, you might want to handle additional messages here
         });
 
         this.on('close', function() {
@@ -65,10 +74,10 @@ module.exports = function(RED) {
     }
     RED.nodes.registerType("ssh-connection-open", SSHInteractiveNode,{
         credentials: {
-            username: {type:"text"},
-            password: {type:"password"},
-            passphrase: {type:"password"},
-            privateKey: {type:"text"}
+            username: {type: "text"},
+            password: {type: "password"},
+            passphrase: {type: "password"},
+            privateKey: {type: "text"}
         }
     });
 };
